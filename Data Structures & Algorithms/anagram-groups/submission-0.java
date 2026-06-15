bce65fe417add3b7c9e7b70271d8d61ceeb939f7
Anshul Ojha <anshulojha091@gmail.com>
2026-06-15 20:23:20 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n = strs.length();
+
+        HashMap<Character, List<String>> map = new HashMap<>();
+
+            
+
+}
+}
